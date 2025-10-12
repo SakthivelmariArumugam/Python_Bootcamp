9bbf231edcd651dbf4edbd01930c0a318bf1e159
Sakthivelamari <sakthivelmaribe@gmail.com>
2025-10-12 18:37:45 +0530

    the cylinder class addded in this file

--- oop_HomeWork.py
+++ oop_HomeWork.py
@@ -14,4 +14,18 @@ class Problem1:
 p1=Problem1((3,2),(8,10))
 p1.distance()
 p1.slope()
+
+class Cylinder:
+  def __init__(self,height=1,radius=1):
+    self.height=height
+    self.radius=radius
   
+  def volume(self):
+    print(2*math.pi*self.radius*self.radius)
+  
+  def surface_area(self):
+    print((2*math.pi*self.radius*(self.radius+self.height)))
+
+c1=Cylinder(2,3)
+c1.volume()
+c1.surface_area()
